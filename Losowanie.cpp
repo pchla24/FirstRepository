8ccc5b7e15308f533d466f31f676317792e98566
pchla24 <janusz_sawicki97@o2.pl>
2017-03-03 19:33:59 +0100

    Add files via upload

---
+++ Losowanie.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+
+{
+	
+	srand( time( NULL ) );
+    std::cout<<(( std::rand() % 1000 ) + 1 ) << std::endl;
+	int wynik = (std::rand() % 1000 ) + 1 ;                   //losowanie
+	
+	int liczba;                                               
+	int i = 0;                                                //Liczba "strza³ów"
+	
+	do
+	{
+
+	i++;
+
+	std::cin>>liczba;
+	
+	if(liczba != wynik)
+		
+		{
+		if(liczba>wynik)
+			std::cout<<"Za duzo"<<std::endl;
+		else
+			std::cout<<"Za malo"<<std::endl;
+		}
+			
+	else
+		std::cout<<"Dobrze, szukana liczba to "<<wynik<<" ,probowales " <<i<< " razy";
+		
+	} while(liczba != wynik);
+	
+	
+	 return 0;
+		}
+	
+	
+	
+
